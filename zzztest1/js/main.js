10a67500c41f4bbb7110b102bf756e2f8c9c8e2c
Christine <christine-xia@hotmail.com>
2020-02-25 03:39:57 -0500

    Move test files to main, main to old

---
+++ zzztest1/js/main.js
@@ -0,0 +1,645 @@
+
+// CLICK TO ANOTHER PAGE
+
+$('.to-vernacular').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.ee').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.ee').css({'font-style':'italic'});
+})
+
+$('.to-da-ge').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.cc').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.cc').css({'font-style':'italic'});
+})
+
+
+// CLICK TO VIEW SECTION
+
+$('li.about').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.about').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('li.contact').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.contact').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('li.main').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.main').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('li.extra').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.extra').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+		// WORKS (CLICK TEXT)
+
+$('span.b.aa').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.aa').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.bb').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.bb').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.cc').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.cc').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.dd').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.dd').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.ee').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.ee').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.ff').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.ff').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.gg').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.gg').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.hh').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.hh').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.ii').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.ii').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.jj').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.jj').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.kk').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.kk').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.ll').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.ll').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.mm').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.mm').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.nn').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.nn').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.oo').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.oo').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+$('span.b.pp').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.pp').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$(this).css({'font-style':'italic'});
+})
+
+		// WORKS (CLICK IMAGE)
+
+$('img.main.aa').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.aa').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.aa').css({'font-style':'italic'});
+})
+
+$('img.main.bb').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.bb').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.bb').css({'font-style':'italic'});
+})
+
+$('img.main.cc').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.cc').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.cc').css({'font-style':'italic'});
+})
+
+$('img.main.dd').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.dd').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.dd').css({'font-style':'italic'});
+})
+
+$('img.main.ee').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.ee').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.ee').css({'font-style':'italic'});
+})
+
+$('img.main.ff').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.ff').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.ff').css({'font-style':'italic'});
+})
+
+$('img.main.gg').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.gg').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.gg').css({'font-style':'italic'});
+})
+
+$('img.main.hh').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.hh').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.hh').css({'font-style':'italic'});
+})
+
+$('img.main.ii').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.ii').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.ii').css({'font-style':'italic'});
+})
+
+$('img.main.jj').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.jj').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.jj').css({'font-style':'italic'});
+})
+
+$('img.main.kk').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.kk').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.kk').css({'font-style':'italic'});
+})
+
+$('img.main.ll').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.ll').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.ll').css({'font-style':'italic'});
+})
+
+$('img.main.mm').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.mm').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.mm').css({'font-style':'italic'});
+})
+
+$('img.main.nn').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.nn').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.nn').css({'font-style':'italic'});
+})
+
+$('img.main.oo').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.oo').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.oo').css({'font-style':'italic'});
+})
+
+$('img.main.pp').click(function(){
+	$('.content').css({'display':'none'});
+	$('.content.pp').css({'display':'inline'});
+	$('li').css({'font-style':'normal'})
+	$('span.b').css({'font-style':'normal'})
+	$('span.b.pp').css({'font-style':'italic'});
+})
+
+
+
+
+
+
+// HOVER OVER LEFT COLUMN TEXT, FLICKER CORRESPONDING RIGHT COLUMN IMAGE
+
+$('span.b.aa').hover(
+	function(){
+		$('img.aa').addClass('flicker-img');
+		$('span.date.aa').css({'display':'inline'});
+	}, function(){
+		$('img.aa').removeClass('flicker-img');
+		$('span.date.aa').css({'display':'none'});
+	}
+)
+
+$('span.b.bb').hover(
+	function(){
+		$('img.bb').addClass('flicker-img');
+		$('span.date.bb').css({'display':'inline'});
+	}, function(){
+		$('img.bb').removeClass('flicker-img');
+		$('span.date.bb').css({'display':'none'});
+	}
+)
+
+$('span.b.cc').hover(
+	function(){
+		$('img.cc').addClass('flicker-img');
+		$('span.date.cc').css({'display':'inline'});
+	}, function(){
+		$('img.cc').removeClass('flicker-img');
+		$('span.date.cc').css({'display':'none'});
+	}
+)
+
+$('span.b.dd').hover(
+	function(){
+		$('img.dd').addClass('flicker-img');
+		$('span.date.dd').css({'display':'inline'});
+	}, function(){
+		$('img.dd').removeClass('flicker-img');
+		$('span.date.dd').css({'display':'none'});
+	}
+)
+
+$('span.b.ee').hover(
+	function(){
+		$('img.ee').addClass('flicker-img');
+		$('span.date.ee').css({'display':'inline'});
+	}, function(){
+		$('img.ee').removeClass('flicker-img');
+		$('span.date.ee').css({'display':'none'});
+	}
+)
+
+$('span.b.ff').hover(
+	function(){
+		$('img.ff').addClass('flicker-img');
+		$('span.date.ff').css({'display':'inline'});
+	}, function(){
+		$('img.ff').removeClass('flicker-img');
+		$('span.date.ff').css({'display':'none'});
+	}
+)
+
+$('span.b.gg').hover(
+	function(){
+		$('img.gg').addClass('flicker-img');
+		$('span.date.gg').css({'display':'inline'});
+	}, function(){
+		$('img.gg').removeClass('flicker-img');
+		$('span.date.gg').css({'display':'none'});
+	}
+)
+
+$('span.b.hh').hover(
+	function(){
+		$('img.hh').addClass('flicker-img');
+		$('span.date.hh').css({'display':'inline'});
+	}, function(){
+		$('img.hh').removeClass('flicker-img');
+		$('span.date.hh').css({'display':'none'});
+	}
+)
+
+$('span.b.ii').hover(
+	function(){
+		$('img.ii').addClass('flicker-img');
+		$('span.date.ii').css({'display':'inline'});
+	}, function(){
+		$('img.ii').removeClass('flicker-img');
+		$('span.date.ii').css({'display':'none'});
+	}
+)
+
+$('span.b.jj').hover(
+	function(){
+		$('img.jj').addClass('flicker-img');
+		$('span.date.jj').css({'display':'inline'});
+	}, function(){
+		$('img.jj').removeClass('flicker-img');
+		$('span.date.jj').css({'display':'none'});
+	}
+)
+
+$('span.b.kk').hover(
+	function(){
+		$('img.kk').addClass('flicker-img');
+		$('span.date.kk').css({'display':'inline'});
+	}, function(){
+		$('img.kk').removeClass('flicker-img');
+		$('span.date.kk').css({'display':'none'});
+	}
+)
+
+$('span.b.ll').hover(
+	function(){
+		$('img.ll').addClass('flicker-img');
+		$('span.date.ll').css({'display':'inline'});
+	}, function(){
+		$('img.ll').removeClass('flicker-img');
+		$('span.date.ll').css({'display':'none'});
+	}
+)
+
+$('span.b.mm').hover(
+	function(){
+		$('img.mm').addClass('flicker-img');
+		$('span.date.mm').css({'display':'inline'});
+	}, function(){
+		$('img.mm').removeClass('flicker-img');
+		$('span.date.mm').css({'display':'none'});
+	}
+)
+
+$('span.b.nn').hover(
+	function(){
+		$('img.nn').addClass('flicker-img');
+		$('span.date.nn').css({'display':'inline'});
+	}, function(){
+		$('img.nn').removeClass('flicker-img');
+		$('span.date.nn').css({'display':'none'});
+	}
+)
+
+$('span.b.oo').hover(
+	function(){
+		$('img.oo').addClass('flicker-img');
+		$('span.date.oo').css({'display':'inline'});
+	}, function(){
+		$('img.oo').removeClass('flicker-img');
+		$('span.date.oo').css({'display':'none'});
+	}
+)
+
+$('span.b.pp').hover(
+	function(){
+		$('img.pp').addClass('flicker-img');
+		$('span.date.pp').css({'display':'inline'});
+	}, function(){
+		$('img.pp').removeClass('flicker-img');
+		$('span.date.pp').css({'display':'none'});
+	}
+)
+
+
+
+
+// HOVER OVER RIGHT COLUMN IMAGE, FLICKER LEFT COLUMN TEXT
+
+$('img.aa').hover(
+	function(){
+		$('span.b.aa').addClass('flicker-txt');
+		$('span.date.aa').css({'display':'inline'});
+	}, function(){
+		$('span.b.aa').removeClass('flicker-txt');
+		$('span.date.aa').css({'display':'none'});
+	}
+)
+
+$('img.bb').hover(
+	function(){
+		$('span.b.bb').addClass('flicker-txt');
+		$('span.date.bb').css({'display':'inline'});
+	}, function(){
+		$('span.b.bb').removeClass('flicker-txt');
+		$('span.date.bb').css({'display':'none'});
+	}
+)
+
+$('img.cc').hover(
+	function(){
+		$('span.b.cc').addClass('flicker-txt');
+		$('span.date.cc').css({'display':'inline'});
+	}, function(){
+		$('span.b.cc').removeClass('flicker-txt');
+		$('span.date.cc').css({'display':'none'});
+	}
+)
+
+$('img.dd').hover(
+	function(){
+		$('span.b.dd').addClass('flicker-txt');
+		$('span.date.dd').css({'display':'inline'});
+	}, function(){
+		$('span.b.dd').removeClass('flicker-txt');
+		$('span.date.dd').css({'display':'none'});
+	}
+)
+
+$('img.ee').hover(
+	function(){
+		$('span.b.ee').addClass('flicker-txt');
+		$('span.date.ee').css({'display':'inline'});
+	}, function(){
+		$('span.b.ee').removeClass('flicker-txt');
+		$('span.date.ee').css({'display':'none'});
+	}
+)
+
+$('img.ff').hover(
+	function(){
+		$('span.b.ff').addClass('flicker-txt');
+		$('span.date.ff').css({'display':'inline'});
+	}, function(){
+		$('span.b.ff').removeClass('flicker-txt');
+		$('span.date.ff').css({'display':'none'});
+	}
+)
+
+$('img.gg').hover(
+	function(){
+		$('span.b.gg').addClass('flicker-txt');
+		$('span.date.gg').css({'display':'inline'});
+	}, function(){
+		$('span.b.gg').removeClass('flicker-txt');
+		$('span.date.gg').css({'display':'none'});
+	}
+)
+
+$('img.hh').hover(
+	function(){
+		$('span.b.hh').addClass('flicker-txt');
+		$('span.date.hh').css({'display':'inline'});
+	}, function(){
+		$('span.b.hh').removeClass('flicker-txt');
+		$('span.date.hh').css({'display':'none'});
+	}
+)
+
+$('img.ii').hover(
+	function(){
+		$('span.b.ii').addClass('flicker-txt');
+		$('span.date.ii').css({'display':'inline'});
+	}, function(){
+		$('span.b.ii').removeClass('flicker-txt');
+		$('span.date.ii').css({'display':'none'});
+	}
+)
+
+$('img.jj').hover(
+	function(){
+		$('span.b.jj').addClass('flicker-txt');
+		$('span.date.jj').css({'display':'inline'});
+	}, function(){
+		$('span.b.jj').removeClass('flicker-txt');
+		$('span.date.jj').css({'display':'none'});
+	}
+)
+
+$('img.kk').hover(
+	function(){
+		$('span.b.kk').addClass('flicker-txt');
+		$('span.date.kk').css({'display':'inline'});
+	}, function(){
+		$('span.b.kk').removeClass('flicker-txt');
+		$('span.date.kk').css({'display':'none'});
+	}
+)
+
+$('img.ll').hover(
+	function(){
+		$('span.b.ll').addClass('flicker-txt');
+		$('span.date.ll').css({'display':'inline'});
+	}, function(){
+		$('span.b.ll').removeClass('flicker-txt');
+		$('span.date.ll').css({'display':'none'});
+	}
+)
+
+$('img.mm').hover(
+	function(){
+		$('span.b.mm').addClass('flicker-txt');
+		$('span.date.mm').css({'display':'inline'});
+	}, function(){
+		$('span.b.mm').removeClass('flicker-txt');
+		$('span.date.mm').css({'display':'none'});
+	}
+)
+
+$('img.nn').hover(
+	function(){
+		$('span.b.nn').addClass('flicker-txt');
+		$('span.date.nn').css({'display':'inline'});
+	}, function(){
+		$('span.b.nn').removeClass('flicker-txt');
+		$('span.date.nn').css({'display':'none'});
+	}
+)
+
+$('img.oo').hover(
+	function(){
+		$('span.b.oo').addClass('flicker-txt');
+		$('span.date.oo').css({'display':'inline'});
+	}, function(){
+		$('span.b.oo').removeClass('flicker-txt');
+		$('span.date.oo').css({'display':'none'});
+	}
+)
+
+$('img.pp').hover(
+	function(){
+		$('span.b.pp').addClass('flicker-txt');
+		$('span.date.pp').css({'display':'inline'});
+	}, function(){
+		$('span.b.pp').removeClass('flicker-txt');
+		$('span.date.pp').css({'display':'none'});
+	}
+)
