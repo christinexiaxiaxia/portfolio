44d7a2ff519546cc55bc217fcee44f7bafc19776
christinexiaxiaxia <christine-xia@hotmail.com>
2026-07-05 18:22:51 -0400

    update html css jquery

---
+++ commercial/js/commercial.js
@@ -0,0 +1,248 @@
+var vid19991 = document.getElementById("vid-1999-1");   
+
+const video = [
+	document.getElementById("vid0"),
+	document.getElementById("vid1"),
+	document.getElementById("vid2"),
+	document.getElementById("vid3"),
+	document.getElementById("vid4"),
+	document.getElementById("vid5"),
+	document.getElementById("vid6"),
+	document.getElementById("vid7"),
+	document.getElementById("vid8"),
+	document.getElementById("vid9"),
+	document.getElementById("vid10"),
+	document.getElementById("vid11"),
+	document.getElementById("vid12")
+]
+
+$(document).ready(function(){
+	$('.mute').toggleClass('hidden')
+})
+
+// for (i = 0; i < 26; i++) {
+
+// 	$(".unmute"+i).click(function(){
+// 		$(this).toggleClass('hidden')
+// 		$(".mute"+i).toggleClass('hidden')
+// 	})
+
+// 	$(".mute"+i).click(function(){
+// 		$(this).toggleClass('hidden')
+// 		$(".unmute"+i).toggleClass('hidden')
+// 	})
+
+// 	$("#mute-button-"+i).on("click", function(){
+// 		video[i].muted = !video[i].muted;
+// 	});
+
+// }
+
+
+
+$(".unmute0").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute0").toggleClass('hidden')
+})
+
+$(".mute0").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute0").toggleClass('hidden')
+})
+
+$("#mute-button-0").on("click", function(e){
+	video[0].muted = !video[0].muted;
+});
+
+
+
+$(".unmute1").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute1").toggleClass('hidden')
+})
+
+$(".mute1").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute1").toggleClass('hidden')
+})
+
+$("#mute-button-1").on("click", function(e){
+	video[1].muted = !video[1].muted;
+});
+
+
+
+$(".unmute2").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute2").toggleClass('hidden')
+})
+
+$(".mute2").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute2").toggleClass('hidden')
+})
+
+$("#mute-button-2").on("click", function(e){
+	video[2].muted = !video[2].muted;
+});
+
+
+
+$(".unmute3").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute3").toggleClass('hidden')
+})
+
+$(".mute3").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute3").toggleClass('hidden')
+})
+
+$("#mute-button-3").on("click", function(e){
+	video[3].muted = !video[3].muted;
+});
+
+
+
+$(".unmute4").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute4").toggleClass('hidden')
+})
+
+$(".mute4").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute4").toggleClass('hidden')
+})
+
+$("#mute-button-4").on("click", function(e){
+	video[4].muted = !video[4].muted;
+});
+
+
+
+$(".unmute5").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute5").toggleClass('hidden')
+})
+
+$(".mute5").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute5").toggleClass('hidden')
+})
+
+$("#mute-button-5").on("click", function(e){
+	video[5].muted = !video[5].muted;
+});
+
+
+
+$(".unmute6").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute6").toggleClass('hidden')
+})
+
+$(".mute6").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute6").toggleClass('hidden')
+})
+
+$("#mute-button-6").on("click", function(e){
+	video[6].muted = !video[6].muted;
+});
+
+
+
+$(".unmute7").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute7").toggleClass('hidden')
+})
+
+$(".mute7").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute7").toggleClass('hidden')
+})
+
+$("#mute-button-7").on("click", function(e){
+	video[7].muted = !video[7].muted;
+});
+
+
+
+$(".unmute8").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute8").toggleClass('hidden')
+})
+
+$(".mute8").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute8").toggleClass('hidden')
+})
+
+$("#mute-button-8").on("click", function(e){
+	video[8].muted = !video[8].muted;
+});
+
+
+
+$(".unmute9").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute9").toggleClass('hidden')
+})
+
+$(".mute9").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute9").toggleClass('hidden')
+})
+
+$("#mute-button-9").on("click", function(e){
+	video[9].muted = !video[9].muted;
+});
+
+
+
+$(".unmute10").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute10").toggleClass('hidden')
+})
+
+$(".mute10").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute10").toggleClass('hidden')
+})
+
+$("#mute-button-10").on("click", function(e){
+	video[10].muted = !video[10].muted;
+});
+
+
+
+$(".unmute11").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute11").toggleClass('hidden')
+})
+
+$(".mute11").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute11").toggleClass('hidden')
+})
+
+$("#mute-button-11").on("click", function(e){
+	video[11].muted = !video[11].muted;
+});
+
+
+
+$(".unmute12").click(function(){
+	$(this).toggleClass('hidden')
+	$(".mute12").toggleClass('hidden')
+})
+
+$(".mute12").click(function(){
+	$(this).toggleClass('hidden')
+	$(".unmute12").toggleClass('hidden')
+})
+
+$("#mute-button-12").on("click", function(e){
+	video[12].muted = !video[12].muted;
+});
+
